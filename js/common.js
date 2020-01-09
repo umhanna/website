65a180aa829dd960e56d6581f6f3a038b02c323b
uhn0753 <36092353+uhn0753@users.noreply.github.com>
2020-01-09 15:01:27 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -13,9 +13,6 @@ $(window).load(function() {
         y = imgStack[minIndex];
         imgStack[minIndex] += ($(imgList[i]).find("img").height() + 150);
         $(imgList[i]).css({top : `${y}px`});
-        $(window).resize(function(){
-            $(imgList[i]).css({top : `${y}px`});
-        });
         if(i === imgList.length - 1) {
         $(".grid").css({height : `${Math.max.apply(0, imgStack)}px`});
         $(window).resize(function(){
